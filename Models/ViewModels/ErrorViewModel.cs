633bc3f94257ec6b0fad6169847ecba92313e7e4
Sapreet Singh <sapreet.singh@autviz.in>
2024-05-19 22:59:45 +0530

    Add project files.

---
+++ Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+
+namespace BookShoping.Model.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
