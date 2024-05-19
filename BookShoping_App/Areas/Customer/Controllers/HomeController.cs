ecd7b1f7b3740ec5c300224b9a9a878b8bb3eadd
Sapreet Singh <sapreet.singh@autviz.in>
2024-05-19 23:26:07 +0530

    project setup

--- BookShoping_App/Areas/Customer/Controllers/HomeController.cs
+++ BookShoping_App/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BookShoping_App.Controllers
+namespace BookShoping_App.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
