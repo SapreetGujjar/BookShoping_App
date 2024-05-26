ccaf5bf842deda74d0e91f2bc2094650c419ba8a
Sapreet Singh <sapreet.singh@autviz.in>
2024-05-26 16:11:49 +0530

    implement generic repository pattern

---
+++ Data/Repository/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookShhoping.ApplicationDb.Repository.IRepository;
+using BookShoping.Model;
+using BookShoping_App.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShhoping.ApplicationDb.Repository
+{
+    public class CategoryRepository : Repository<Category>,ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context):base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookShoping.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShhoping.ApplicationDb.Repository.IRepository
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+    }
+}
